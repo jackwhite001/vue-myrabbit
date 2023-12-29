01122e38d14fbe42178bff93b010d520c8ba2091
jackwhite001 <1121176166@qq.com>
2023-12-29 15:25:34 +0800

    axios请求

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
-
+// 导入测试接口函数
+import { getCategory } from '@/apis/testAPI'
+getCategory().then(res=>{
+  console.log(res);
+})
 const app = createApp(App)
 const pinia = createPinia()
 app.use(ElementPlus) //插件式全局注册
