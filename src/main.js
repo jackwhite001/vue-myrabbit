3707ef46f8ac65a27a77b8a900b9bdf41dfea64d
jackwhite001 <1121176166@qq.com>
2023-12-29 14:51:41 +0800

    element主题颜色

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+app.use(ElementPlus) //插件式全局注册
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// Element-Plus 按需引入
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,10 +11,30 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    //导入的 插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 1、配置elementPlus采用scss样式颜色系统 
+        ElementPlusResolver({ importStyle: "sass"})
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          `,          //@use "@/styles/var.scss" as *;
+      }
     }
   }
 })
